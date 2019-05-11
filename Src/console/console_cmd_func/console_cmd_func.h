70357dc3f932dcf7d23c9c0c39511fd4421077b9
Viacheslav <viacheslav@mcublog.xyz>
2019-05-11 14:38:15 +0300

    WIP; delete unused files;

--- Src/console/console_cmd_func/console_cmd_func.h
+++ Src/console/console_cmd_func/console_cmd_func.h
@@ -4,11 +4,7 @@
 #include "cmsis_os.h"
 
 #include "console_data_parse/console_data_parse.h"
-#include "console_data_ctrl/console_data_ctrl.h"
-
-extern void ReportServiceTask	   (void *pvParameters);//сервисная задача для акс
-
-extern TaskHandle_t xReportServiceHandle;
+#include "io_console.h"
 
 void cmd_fuction_caller(console_cmd_t* cl_cmd, console_data_ctrl_t *con_h);
 
